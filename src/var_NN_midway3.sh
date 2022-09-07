79fe8f75ad6273bebd2e2104a65e43aae9b8bf1c
hqin-2020 <uctphqi@ucl.ac.uk>
2022-09-07 15:40:08 -0500

    1

--- src/var_NN_midway3.sh
+++ src/var_NN_midway3.sh
@@ -27,13 +27,13 @@ fi
 
 for chiUnderline in 1.0
 do 
-    for a_e in 0.15
+    for a_e in 0.14
     do
-        for a_h in 0.13 0.1 0.07 0.05
+        for a_h in 0.135
         do
-            for gamma_e in 1.0
+            for gamma_e in 0.5
             do
-                for gamma_h in 1.0
+                for gamma_h in 8.0
                 do
                     for psi_e in 1.0
                     do
@@ -82,12 +82,13 @@ do
 #SBATCH --output=./job-outs/$domain_folder/$model_folder/$layer_folder/var.out
 #SBATCH --error=./job-outs/$domain_folder/$model_folder/$layer_folder/var.err
 #SBATCH --time=0-24:00:00
-#SBATCH --partition=caslake
-#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=14
-#SBATCH --mem=56G
+#SBATCH --mem=64G
 
 module load python/anaconda-2021.05
+module load cuda/11.5
 
 python3 WZVtilde_variable.py   --chiUnderline ${chiUnderline} --a_e ${a_e} --a_h ${a_h} --gamma_e ${gamma_e} --gamma_h ${gamma_h} --psi_e ${psi_e} --psi_h ${psi_h} --nWealth ${nWealth} --nZ ${nZ} --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} --XiE_layers ${XiE_layers} --XiH_layers ${XiH_layers} --kappa_layers ${kappa_layers} --weight1 ${weight1} --boundary1 ${boundary1} --weight2 ${weight2} --boundary2 ${boundary2} --points_size ${points_size} --iter_num ${iter_num} 
 
